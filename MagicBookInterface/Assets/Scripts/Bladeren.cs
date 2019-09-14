a2f6063cfabf0fed687abb95665932fd8918ae4b
SuryaniD <bytebytermail@gmail.com>
2019-09-14 20:54:25 +0200

    Made beginning of the book interface

---
+++ MagicBookInterface/Assets/Scripts/Bladeren.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bladeren : MonoBehaviour
+{
+    int startPage;
+    public GameObject PageLeft;
+    public GameObject PageRight;
+    public GameObject PageFlip;
+    Texture2D pageTexture;
+	Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPage = 1;
+
+		anim = this.GetComponent<Animator>();
+
+		PageFlip.SetActive(false);
+
+        pageTexture = Resources.Load("page" + startPage) as Texture2D;
+        PageLeft.GetComponent<Renderer>().material.mainTexture = pageTexture;
+
+		pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+		PageRight.GetComponent<Renderer>().material.mainTexture = pageTexture;
+
+		pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+        PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
+
+        pageTexture = Resources.Load("page" + (startPage + 2)) as Texture2D;
+        PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown("up"))
+		{
+
+			Debug.Log("Start1 = "+startPage);
+
+			pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+			PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
+
+			pageTexture = Resources.Load("page" + (startPage + 2)) as Texture2D;
+			PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+
+			startPage++;
+
+			Debug.Log("Start2 = "+startPage);
+
+			pageTexture = Resources.Load("page" + startPage) as Texture2D;
+			PageLeft.GetComponent<Renderer>().material.mainTexture = pageTexture;
+
+			pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+			PageRight.GetComponent<Renderer>().material.mainTexture = pageTexture;
+
+			PageFlip.SetActive(true);
+
+			anim.Play("PageForward",-1,0f);
+
+		}
+
+		if (Input.GetKeyDown("down"))
+		{
+			startPage--;
+		}
+
+    }
+}
--- MagicBookInterface/Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ MagicBookInterface/Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -51,6 +51,10 @@ namespace Valve.VR
         
         private static SteamVR_Action_Pose p_mixedreality_ExternalCamera;
         
+        private static SteamVR_Action_Boolean p_bookinterface_FlipForward;
+        
+        private static SteamVR_Action_Boolean p_bookinterface_FlipBack;
+        
         public static SteamVR_Action_Boolean default_InteractUI
         {
             get
@@ -187,6 +191,22 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Boolean bookinterface_FlipForward
+        {
+            get
+            {
+                return SteamVR_Actions.p_bookinterface_FlipForward.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
+        public static SteamVR_Action_Boolean bookinterface_FlipBack
+        {
+            get
+            {
+                return SteamVR_Actions.p_bookinterface_FlipBack.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
         private static void InitializeActionArrays()
         {
             Valve.VR.SteamVR_Input.actions = new Valve.VR.SteamVR_Action[] {
@@ -206,7 +226,9 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.mixedreality_ExternalCamera};
+                    SteamVR_Actions.mixedreality_ExternalCamera,
+                    SteamVR_Actions.bookinterface_FlipForward,
+                    SteamVR_Actions.bookinterface_FlipBack};
             Valve.VR.SteamVR_Input.actionsIn = new Valve.VR.ISteamVR_Action_In[] {
                     SteamVR_Actions.default_InteractUI,
                     SteamVR_Actions.default_Teleport,
@@ -223,7 +245,9 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset,
-                    SteamVR_Actions.mixedreality_ExternalCamera};
+                    SteamVR_Actions.mixedreality_ExternalCamera,
+                    SteamVR_Actions.bookinterface_FlipForward,
+                    SteamVR_Actions.bookinterface_FlipBack};
             Valve.VR.SteamVR_Input.actionsOut = new Valve.VR.ISteamVR_Action_Out[] {
                     SteamVR_Actions.default_Haptic};
             Valve.VR.SteamVR_Input.actionsVibration = new Valve.VR.SteamVR_Action_Vibration[] {
@@ -239,7 +263,9 @@ namespace Valve.VR
                     SteamVR_Actions.default_HeadsetOnHead,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Brake,
-                    SteamVR_Actions.buggy_Reset};
+                    SteamVR_Actions.buggy_Reset,
+                    SteamVR_Actions.bookinterface_FlipForward,
+                    SteamVR_Actions.bookinterface_FlipBack};
             Valve.VR.SteamVR_Input.actionsSingle = new Valve.VR.SteamVR_Action_Single[] {
                     SteamVR_Actions.default_Squeeze,
                     SteamVR_Actions.buggy_Throttle};
@@ -262,7 +288,9 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Steering,
                     SteamVR_Actions.buggy_Throttle,
                     SteamVR_Actions.buggy_Brake,
-                    SteamVR_Actions.buggy_Reset};
+                    SteamVR_Actions.buggy_Reset,
+                    SteamVR_Actions.bookinterface_FlipForward,
+                    SteamVR_Actions.bookinterface_FlipBack};
         }
         
         private static void PreInitActions()
@@ -284,6 +312,8 @@ namespace Valve.VR
             SteamVR_Actions.p_buggy_Brake = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/buggy/in/Brake")));
             SteamVR_Actions.p_buggy_Reset = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/buggy/in/Reset")));
             SteamVR_Actions.p_mixedreality_ExternalCamera = ((SteamVR_Action_Pose)(SteamVR_Action.Create<SteamVR_Action_Pose>("/actions/mixedreality/in/ExternalCamera")));
+            SteamVR_Actions.p_bookinterface_FlipForward = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/bookinterface/in/FlipForward")));
+            SteamVR_Actions.p_bookinterface_FlipBack = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/bookinterface/in/FlipBack")));
         }
     }
 }
