e3dea0ad40a13ef1d56ff3eaf25da7da93132321
SuryaniD <bytebytermail@gmail.com>
2019-09-16 17:42:50 +0200

    Book works with VR controls elemental orbs created, new skybox

--- MagicBookInterface/Assets/Scripts/Bladeren.cs
+++ MagicBookInterface/Assets/Scripts/Bladeren.cs
@@ -1,72 +1,182 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
-public class Bladeren : MonoBehaviour
-{
+public class Bladeren : MonoBehaviour {
+
     int startPage;
+
+	public SteamVR_Action_Boolean flipB;
+	public SteamVR_Action_Boolean flipF;
+
+	public SteamVR_Input_Sources handType;
+
     public GameObject PageLeft;
     public GameObject PageRight;
     public GameObject PageFlip;
     Texture2D pageTexture;
-	Animator anim;
+	public Animator anim;
+	bool OnTheLeft;
+
+	public Material AirMaterial;
+	public Material EarthMaterial;
+	public Material FireMaterial;
+	public Material WaterMaterial;
+	
+	
+	public GameObject ForceSphere;
+
+	private void OnEnable() {
+
+		if (flipB == null || flipF == null)	{
+			Debug.LogError("<b>[SteamVR Interaction]</b> No flip action assigned");
+			return;
+		}
+
+		flipB.AddOnChangeListener(OnFlipBActionChange, handType);
+		flipF.AddOnChangeListener(OnFlipFActionChange, handType);
+	}
+
+	private void OnDisable() {
+
+			if (flipB != null)
+				flipB.RemoveOnChangeListener(OnFlipBActionChange, handType);
+			if (flipF != null)
+				flipF.RemoveOnChangeListener(OnFlipFActionChange, handType);
+			
+	}
+	
+	private void ChangeForce() {
+		
+		if (startPage==1)
+			ForceSphere.GetComponent<Renderer>().material = EarthMaterial;
+		else if (startPage==3)
+			ForceSphere.GetComponent<Renderer>().material = WaterMaterial;
+		else if (startPage==5)
+			ForceSphere.GetComponent<Renderer>().material = AirMaterial;
+		else if (startPage==7)
+			ForceSphere.GetComponent<Renderer>().material = FireMaterial;
+
+	}
+	
+	private void OnFlipBActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue) {
+		
+		if (newValue) {
+			
+			if (anim.GetBool ("Flipped") && startPage>2) {
+				
+				startPage -= 2;
+
+				anim.SetBool("Flipped", false);
+
+				ForceSphere.SetActive(false);
+
+				ChangeForce();
+				
+				pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+				PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
+
+				pageTexture = Resources.Load("page" + (startPage + 2)) as Texture2D;
+				PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+
+				PageFlip.SetActive(true);
+
+				anim.Play("FlipBack",-1,0f);
+
+				OnTheLeft = false;
+
+				pageTexture = Resources.Load("page" + startPage) as Texture2D;
+				PageLeft.GetComponent<Renderer>().material.mainTexture = pageTexture;
+
+			}
+		}
+	}
+
+	private void OnFlipFActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue) {
+		
+		if (newValue) {
+			
+			if (anim.GetBool ("Flipped") && startPage<6) {
+				
+				startPage += 2;
+
+				anim.SetBool("Flipped", false);
+
+				ForceSphere.SetActive(false);
+
+				ChangeForce();
+				
+				pageTexture = Resources.Load("page" + startPage) as Texture2D;
+				PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+
+				pageTexture = Resources.Load("page" + (startPage - 1)) as Texture2D;
+				PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
+
+				PageFlip.SetActive(true);
+
+				anim.Play("FlipForward",-1,0f);
+
+				OnTheLeft = true;
+
+				pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+				PageRight.GetComponent<Renderer>().material.mainTexture = pageTexture;
+
+			}
+
+		}
+
+	}
+
 
     // Start is called before the first frame update
     void Start()
     {
         startPage = 1;
 
-		anim = this.GetComponent<Animator>();
+		OnTheLeft = false;
 
 		PageFlip.SetActive(false);
 
-        pageTexture = Resources.Load("page" + startPage) as Texture2D;
+		ForceSphere.SetActive(false);
+
+		anim.Play("Flipped",-1,0f);
+
+        pageTexture = Resources.Load("page1") as Texture2D;
         PageLeft.GetComponent<Renderer>().material.mainTexture = pageTexture;
 
-		pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+		pageTexture = Resources.Load("page2") as Texture2D;
 		PageRight.GetComponent<Renderer>().material.mainTexture = pageTexture;
 
-		pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
-        PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
+		pageTexture = Resources.Load("page2") as Texture2D;
+		PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
 
-        pageTexture = Resources.Load("page" + (startPage + 2)) as Texture2D;
-        PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+		pageTexture = Resources.Load("page3") as Texture2D;
+		PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-		if (Input.GetKeyDown("up"))
-		{
+    void Update() {
 
-			Debug.Log("Start1 = "+startPage);
+		if (anim.GetBool ("Flipped") && PageFlip.activeSelf) { //Finished flipping? Load page texture beneath and remove flip page.
 
-			pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
-			PageFlip.GetComponent<Renderer>().materials[0].mainTexture = pageTexture;
+			if (OnTheLeft) {
+				pageTexture = Resources.Load("page" + startPage) as Texture2D;
+				PageLeft.GetComponent<Renderer>().material.mainTexture = pageTexture;
+			} else {
+				pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
+				PageRight.GetComponent<Renderer>().material.mainTexture = pageTexture;
+			}
 
-			pageTexture = Resources.Load("page" + (startPage + 2)) as Texture2D;
-			PageFlip.GetComponent<Renderer>().materials[1].mainTexture = pageTexture;
+			PageFlip.SetActive(false);
 
-			startPage++;
+			ForceSphere.SetActive(true);
 
-			Debug.Log("Start2 = "+startPage);
-
-			pageTexture = Resources.Load("page" + startPage) as Texture2D;
-			PageLeft.GetComponent<Renderer>().material.mainTexture = pageTexture;
-
-			pageTexture = Resources.Load("page" + (startPage + 1)) as Texture2D;
-			PageRight.GetComponent<Renderer>().material.mainTexture = pageTexture;
-
-			PageFlip.SetActive(true);
-
-			anim.Play("PageForward",-1,0f);
-
-		}
-
-		if (Input.GetKeyDown("down"))
-		{
-			startPage--;
 		}
 
     }
+
 }
---
+++ MagicBookInterface/Assets/Scripts/Flipped.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipped : StateMachineBehaviour {
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.SetBool("Flipped", true);
+	}
+}
